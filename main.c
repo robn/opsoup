ead8a06df689c5d6765cbad31aed9250bbe06ac7
Robert Norris <rob@cataclysm.cx>
2008-08-08 07:41:43 +1000

    more old code pruning

--- main.c
+++ main.c
@@ -41,26 +41,12 @@ int main(int argc, char **argv) {
 
     dis_pass3(f);
 
-    /*
     data_output(f);
     data_bss_output(f);
-    */
 
     fclose(f);
 
-    /*
-    f = fopen(OUTPUT_STUB, "w");
-    if(f == NULL) {
-        printf("main: couldn't open '" OUTPUT_STUB "' for writing: %s\n", strerror(errno));
-        return 1;
-    }
-
-    import_stub(f);
-
-    fclose(f);
-
-    label_print_unused();
-    */
+    //label_print_unused();
 
     return 0;
 }
